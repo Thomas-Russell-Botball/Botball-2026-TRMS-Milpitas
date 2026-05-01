394733fa820d90947cae0ad125dd45e0d25e049e
Dhiaan Dave <120334591+the-X-alien@users.noreply.github.com>
2026-05-01 10:31:22 -0700

    m

--- hola.c
+++ hola.c
@@ -1,91 +1,93 @@
+#include <stdio.h>
 #include <kipr/wombat.h>
 #include <stdlib.h>
 
-// Motor ports (per provided mapping)
-#define RIGHT_BACK_MOTOR 0
-#define RIGHT_FRONT_MOTOR 1
-#define LEFT_FRONT_MOTOR 2
-#define LEFT_BACK_MOTOR 3
+#define LEFTMOTOR1 0 //#define RIGHT_BACK_MOTOR 0
+#define LEFTMOTOR2 1 //#define RIGHT_FRONT_MOTOR 1
+#define RIGHTMOTOR1 2 //#define LEFT_BACK_MOTOR 2
+#define RIGHTMOTOR2 3 //#define LEFT_FRONT_MOTOR 3
+#define LIGHTSENSOR1 0
+#define LIGHTSENSOR2 1
+#define compensation_rate 0.2
+#define claw_click 0
 
-// Servo ports
-#define SERVO_FRONT_RIGHT 2
-#define SERVO_FRONT_LEFT 3
-
-// Sensor ports
-// Digital: start light sensor
-#define START_LIGHT_SENSOR 3
-// Analog: tophat sensors for squaring up and line follow
-#define SQUARE_LEFT_SENSOR 2
-#define LINE_SENSOR 1
-#define SQUARE_RIGHT_SENSOR 0
-
-#define LINE_TARGET 3000
-
-void drive(int cm, int speed){
-    float move = 2000*(cm/27);
-    cmpc(LEFT_FRONT_MOTOR);
-    while(abs(gmpc(LEFT_FRONT_MOTOR)) < move){
-        mav(RIGHT_FRONT_MOTOR, -speed);
-        mav(RIGHT_BACK_MOTOR, speed);
-        mav(LEFT_FRONT_MOTOR, speed);
-        mav(LEFT_BACK_MOTOR, speed);
+    void ow_turnr_90() {
+        mav(RIGHTMOTOR1, 1000);
+        mav(RIGHTMOTOR2, 1000);
+ 		mav(LEFTMOTOR1, -1000);
+        mav(LEFTMOTOR2, -1000);
+        msleep(2500);
+       	mav(RIGHTMOTOR1, 0);
+        mav(RIGHTMOTOR2, 0);
+  		mav(LEFTMOTOR1, 0);
+       	mav(LEFTMOTOR2, 0);
+        msleep(20);
     }
-    mav(RIGHT_FRONT_MOTOR, 0);
-    mav(RIGHT_BACK_MOTOR, 0);
-    mav(LEFT_FRONT_MOTOR, 0);
-    mav(LEFT_BACK_MOTOR, 0);
-    msleep(20);
-}
-
-void turnright(float distance, int speed){
-    cmpc(LEFT_BACK_MOTOR);
-    while(fabs(gmpc(LEFT_BACK_MOTOR)) < 1700*distance/90.0){
-        mav(RIGHT_FRONT_MOTOR, -speed);
-        mav(RIGHT_BACK_MOTOR, speed);
-        mav(LEFT_FRONT_MOTOR, speed);
-        mav(LEFT_BACK_MOTOR, -speed);
+    
+	void drive(int cm, int speed){
+        float move = 2000*(cm/27);
+  		cmpc(RIGHTMOTOR1);
+  		while(abs(gmpc(RIGHTMOTOR1)) < move){
+    		mav(RIGHTMOTOR1, -speed);
+        	mav(RIGHTMOTOR2, speed);
+    		mav(LEFTMOTOR1, speed);
+        	mav(LEFTMOTOR2, speed);
+  		}
+  			mav(RIGHTMOTOR1, 0);
+        	mav(RIGHTMOTOR2, 0);
+  			mav(LEFTMOTOR1, 0);
+        	mav(LEFTMOTOR2, 0);
+  			msleep(20);
+		}
+    
+    void turnright(float distance, int speed){
+  		cmpc(LEFTMOTOR1);
+  		while(fabs(gmpc(LEFTMOTOR1)) < 1700*distance/90.0){
+    		mav(RIGHTMOTOR1, -speed);
+            mav(RIGHTMOTOR2, speed);
+    		mav(LEFTMOTOR1, speed);
+            mav(LEFTMOTOR2, -speed);
+  		}
+  		mav(RIGHTMOTOR1, 0);
+        mav(RIGHTMOTOR2, 0);
+  		mav(LEFTMOTOR1, 0);
+       	mav(LEFTMOTOR2, 0);
+  		msleep(20);
     }
-    mav(RIGHT_FRONT_MOTOR, 0);
-    mav(RIGHT_BACK_MOTOR, 0);
-    mav(LEFT_FRONT_MOTOR, 0);
-    mav(LEFT_BACK_MOTOR, 0);
-    msleep(20);
-}
-
-void turnleft(float distance, int speed){
-    cmpc(LEFT_FRONT_MOTOR);
-    while(fabs(gmpc(LEFT_FRONT_MOTOR)) < 1700*distance/90.0){
-        mav(RIGHT_FRONT_MOTOR, speed);
-        mav(RIGHT_BACK_MOTOR, -speed);
-        mav(LEFT_FRONT_MOTOR, -speed);
-        mav(LEFT_BACK_MOTOR,  speed);
+    void turnleft(float distance, int speed){
+  		cmpc(RIGHTMOTOR2);
+  		while(fabs(gmpc(RIGHTMOTOR2)) < 1700*distance/90.0){
+    		mav(RIGHTMOTOR1, speed);
+            mav(RIGHTMOTOR2, -speed);
+    		mav(LEFTMOTOR1, -speed);
+            mav(LEFTMOTOR2,  speed);
+  		}
+  		mav(RIGHTMOTOR1, 0);
+        mav(RIGHTMOTOR2, 0);
+  		mav(LEFTMOTOR1, 0);
+       	mav(LEFTMOTOR2, 0);
+  		msleep(20);
     }
-    mav(RIGHT_FRONT_MOTOR, 0);
-    mav(RIGHT_BACK_MOTOR, 0);
-    mav(LEFT_FRONT_MOTOR, 0);
-    mav(LEFT_BACK_MOTOR, 0);
-    msleep(20);
-}
-
-// Robust line follow (based on Rancho MMS pattern)
-void linefollow(int distance, int speed, int encoder_port, float compensation_rate){
-    cmpc(encoder_port);
-    while (abs(gmpc(encoder_port)) < distance) {
-        int error = analog(LINE_SENSOR) - LINE_TARGET;
+    void linefollow(int distance, int speed, int port,float compensation_rate){
+       cmpc(port);
+        while (abs(gmpc(port)) < distance) {
+        int error = analog(2) - (3000);
         float speed_modifier = error * compensation_rate;
-
-        mav(LEFT_FRONT_MOTOR, speed - speed_modifier);
-        mav(LEFT_BACK_MOTOR, -speed + speed_modifier);
-
-        mav(RIGHT_FRONT_MOTOR, speed + speed_modifier);
-        mav(RIGHT_BACK_MOTOR, -speed - speed_modifier);
+        printf("%i\n",error);
+        mav(LEFTMOTOR1, speed - speed_modifier);
+        mav(LEFTMOTOR2, -speed + speed_modifier);
+        
+        mav(RIGHTMOTOR1, speed + speed_modifier);
+        mav(RIGHTMOTOR2, -speed - speed_modifier);
+        }
+        freeze(0);
+        freeze(1);
+        freeze(2);
+        freeze(3);
+  
     }
-    mav(RIGHT_FRONT_MOTOR, 0);
-    mav(RIGHT_BACK_MOTOR, 0);
-    mav(LEFT_FRONT_MOTOR, 0);
-    mav(LEFT_BACK_MOTOR, 0);
-    msleep(20);
-}
+    
+
 
 void pidMove(float distance, float topSpeed,float finalError){
     cmpc(0); cmpc(1); cmpc(2); cmpc(3);
@@ -99,96 +101,106 @@ void pidMove(float distance, float topSpeed,float finalError){
     float integral = 0;
     float dt =20;
 
-    while(fabsf(error)>finalError){
-        //1. Get input
-        float position = (gmpc(0)-gmpc(1)+gmpc(2)-gmpc(3));
-        //40 cm is 1500
-        position/=4;
-        position/=2;
-        position/=37.5;
+        while(fabsf(error)>finalError){
+            //1. Get input
+            float position = (gmpc(0)-gmpc(1)+gmpc(2)-gmpc(3));
+            //40 cm is 1500
+            position/=4;
+            position/=2;
+            position/=37.5;
+            
+            //2. Calculate error
+            error = distance - position;
+            /* printf("%i,",gmpc(0));
+             printf("%i,",gmpc(1));
+             printf("%i,",gmpc(2));
+             printf("%i,",gmpc(3));
+            printf("\n");
+            printf("%f",position);
+            printf("\n");*/
 
-        //2. Calculate error
-        error = distance - position;
+            
+            
+            //3. Calculate terms
+            integral = integral + (error * dt);
+            if(integral/fabs(integral)!=error/fabs(error)){
+                integral=0;
+            }
+           
+ 
+               
+            int derivative = (error - lastError) / dt;
+            
+            //4. Calculate output
+            float output = (KP * error) + (KI * integral) - (KD * derivative);
 
-        //3. Calculate terms
-        integral = integral + (error * dt);
-        if(integral/fabs(integral)!=error/fabs(error)){
-            integral=0;
-        }
-
-        int derivative = (error - lastError) / dt;
-
-        //4. Calculate output
-        float output = (KP * error) + (KI * integral) - (KD * derivative);
-
-        //Makes sure speed isn't over top speed
-        if(fabs(output)>topSpeed){
+            //Makes sure speed isn't over top speed
+            if(fabs(output)>topSpeed){
             output/=fabs(output);
-            output*= topSpeed;
+            output*= topSpeed;        
+            }
+            
+            //5. Apply output and save state  
+            mav(0,output);
+            mav(1,-output);
+            mav(2,output);
+            mav(3,-output);
+            lastError = error;
+            
+            //6. Wait for next time step
+            msleep(dt);
+
         }
-
-        //5. Apply output and save state
-        mav(0,output);
-        mav(1,-output);
-        mav(2,output);
-        mav(3,-output);
-        lastError = error;
-
-        //6. Wait for next time step
-        msleep(dt);
-    }
     freeze(0);
     freeze(1);
     freeze(2);
     freeze(3);
 }
 
+
+
+
 int main()
 {
-    enable_servos();
-    set_servo_position(0,780);//780 is Up 2047 is down
-    set_servo_position(1,0);
-    set_servo_position(2,2300);
-    set_servo_position(3,2300);
+    	enable_servos();
+        //set_servo_position(0,780);//780 is Up 2047 is down
+        //set_servo_position(1,0);
+        set_servo_position(2,2300);
+        set_servo_position(3,2300);
 
-    msleep(200);
+   msleep(200);
     //move forward 40 cm
-    pidMove(57,1500,0.3);
+    pidMove(70,1500,0.3);
     //turn left 90
-    turnleft(100,1500);
+    turnleft(90,1500);
     msleep(1000);
-
+    
     //square up
-    pidMove(-500,1500,490);
-    //turnleft(8,400);
-
-    //Forward
-
+    pidMove(-500,1500,485);
+    turnright(1,400);
+    //Forward 
+    
     set_servo_position(2,1170);
     set_servo_position(3,1100);
+    	msleep(1000);
 
-    msleep(2000);
-    pidMove(30,1500,0.3);
-    //turnright(2,1500);
-    pidMove(160,1500,0.3);
-    msleep(2000);
+        pidMove(150,1500,0.3);
+    turnleft(2,1500);
+     pidMove(50,1500,0.3);
+	msleep(500);
+    turnright(240,400);
+    turnright(220,400);
+    msleep(1000);
+  
+    pidMove(150,1500,0.3);
 
-    mav(LEFT_FRONT_MOTOR, 1500);
-    mav(LEFT_BACK_MOTOR, -1500);
-    mav(RIGHT_FRONT_MOTOR, 100);
-    mav(RIGHT_BACK_MOTOR, -100);
-    msleep(5150);
-    mav(LEFT_FRONT_MOTOR, 0);
-    mav(LEFT_BACK_MOTOR, 0);
 
-    pidMove(155,1500,0.5);
-    turnright(90,1000);
-    pidMove(20,1500,0.5);
-    turnright(45,1000);
-    pidMove(80,1500,0.5);
+    
+   
 
-    pidMove(-30,1500,0.5);
+
 
     disable_servos();
     return 0;
+
 }
