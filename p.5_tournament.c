34b6ab2178691edf51fa457e6a50c6ed61af542b
Dhiaan Dave <120334591+the-X-alien@users.noreply.github.com>
2026-05-04 11:33:55 -0700

    Add tournament control logic in p.5_tournament.c

---
+++ p.5_tournament.c
@@ -0,0 +1,54 @@
+#include <kipr/wombat.h>
+
+//positive speed value for forward (towards claw) and negative speed value for backwards (towards pusher)
+void f_b(int speed, int time){
+    motor(0, speed);
+    motor(3,speed);
+    msleep(time);
+}
+
+void left(int speed, int time){
+    motor(0,speed);
+    msleep(time);
+}
+
+void right(int speed, int time){
+    motor(3,speed);
+    msleep(time);
+}
+
+//motor(port, power);  |  Use da motor
+//msleep(milliseconds);  |  time the motor/servo has to do their action
+//enable_servo(port);  |  Enable the servo
+//disable_servo(port);  |  Disable the servo
+//set_servo_position(port, position);  |  Set position of a servo
+//ao();  |  Stop all motors
+
+
+int main()
+{
+	left(200,1000);
+    f_b(-200,500);
+    left(200,250);
+    f_b(-200,100);
+    left(200,350);
+    f_b(-200,100);
+    left(200,75);
+    
+    ao();
+    
+    //pushpoms
+    f_b(-350,5000);
+    //comeback
+    f_b(350,5000);
+    ao(); 
+    //rest orientation
+    f_b(200,100);
+    left(-200,350);
+    f_b(200,100);
+    left(-200,250);
+    f_b(200,500);
+    //left(-200,1000);
+    ao(); 
+    return 0;
+}
