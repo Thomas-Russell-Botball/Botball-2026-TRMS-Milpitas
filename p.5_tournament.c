60f7a66ebdf899e2be47ea76b1eb793332574217
Dhiaan Dave <120334591+the-X-alien@users.noreply.github.com>
2026-05-07 14:27:25 -0700

    Refactor motor control functions and add line following

--- p.5_tournament.c
+++ p.5_tournament.c
@@ -1,54 +1,105 @@
 #include <kipr/wombat.h>
+#define BLACK_VALUE 1300
+#define LMOTOR 0
+#define RMOTOR 3
+#define LEFT_TOPHAT 3
+#define RIGHT_TOPHAT 2
 
-//positive speed value for forward (towards claw) and negative speed value for backwards (towards pusher)
-void f_b(int speed, int time){
+#define LEFT_THRESHOLD 330
+#define RIGHT_THRESHOLD 3680
+#define BASE_SPEED 2000
+#define TURN_FACTOR 1
+#define LEFT_TURN_FACTOR 0.7
+
+void forward(int speed, int time) {
     motor(0, speed);
-    motor(3,speed);
+    motor(3, speed);
     msleep(time);
 }
 
-void left(int speed, int time){
-    motor(0,speed);
+void backward(int speed, int time) {
+    motor(0, -speed);
+    motor(3, -speed);
     msleep(time);
 }
 
-void right(int speed, int time){
-    motor(3,speed);
+void right(int speed, int time) {
+    motor(0, speed);
+    motor(3, -speed);
     msleep(time);
 }
 
-//motor(port, power);  |  Use da motor
-//msleep(milliseconds);  |  time the motor/servo has to do their action
-//enable_servo(port);  |  Enable the servo
-//disable_servo(port);  |  Disable the servo
-//set_servo_position(port, position);  |  Set position of a servo
-//ao();  |  Stop all motors
+void left(int speed, int time) {
+    motor(0, -speed);
+    motor(3, speed);
+    msleep(time);
+}
 
 
-int main()
-{
-	left(200,1000);
-    f_b(-200,500);
-    left(200,250);
-    f_b(-200,100);
-    left(200,350);
-    f_b(-200,100);
-    left(200,75);
-    
-    ao();
-    
-    //pushpoms
-    f_b(-350,5500);
-    //comeback
-    f_b(350,5500);
-    ao(); 
-    //rest orientation
-    f_b(200,100);
-    left(-200,350);
-    f_b(200,100);
-    left(-200,250);
-    f_b(200,500);
-    //left(-200,1000);
-    ao(); 
+
+void line_follow(int duration_ms){
+    int elapsed = 0; 
+
+    while(elapsed < duration_ms){
+        int left = analog(LEFT_TOPHAT);
+        int right = analog(RIGHT_TOPHAT);
+
+        if(left < LEFT_THRESHOLD && right < RIGHT_THRESHOLD){  // both black
+            mav(0, -BASE_SPEED);
+            mav(3, -BASE_SPEED);
+        }
+        else if(left < LEFT_THRESHOLD){  // left black= turn left
+            mav(0, -BASE_SPEED * TURN_FACTOR);
+            mav(3, -BASE_SPEED);
+        }
+        else if(right < RIGHT_THRESHOLD){ // right black= turn right
+            mav(0, -BASE_SPEED);
+            mav(3, -BASE_SPEED * TURN_FACTOR);
+        }
+        else{ // lost line= move slowly forward
+            mav(0, -BASE_SPEED * 0.7);
+            mav(3, -BASE_SPEED * 0.7);
+        }
+
+        msleep(10);
+        elapsed += 10;
+    }
+
+    // Stop motors
+    mav(0, 0);
+    mav(3, 0);
+}
+
+
+void servo(int port, int position, float time){
+    float start_time = seconds();
+    int start_pos = get_servo_position(port);
+    int pos_diff = position - start_pos;
+    while(seconds() - start_time < time){
+        float pos_mod = (seconds()-start_time)/time;
+        set_servo_position(port, start_pos + (pos_diff * pos_mod));
+    }
+    set_servo_position(port, position);
+    msleep(50);
+}
+
+
+
+int main() {
+    wait_for_light(5);
+    shut_down_in(118);
+    line_follow(5500);
+    forward(50,6500);
+    right(100,400);
+    backward(100,1000);
+    left(100,450);
+    backward(100,2350);
+    forward(100,6000);
+
+
+    disable_servos();
+
+
+
     return 0;
 }
