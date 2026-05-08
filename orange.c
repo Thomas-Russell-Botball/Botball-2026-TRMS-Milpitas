aea769e9a22e04efba7b49600e01a80347c02430
Dhiaan Dave <120334591+the-X-alien@users.noreply.github.com>
2026-05-07 22:24:27 -0700

    Refactor motor and servo control functions

--- orange.c
+++ orange.c
@@ -1,18 +1,18 @@
 #include <kipr/wombat.h>
+#include <stdlib.h>
 
-// Motor ports (your original)
-#define RIGHT_BACK_MOTOR 0
-#define RIGHT_FRONT_MOTOR 1
-#define LEFT_FRONT_MOTOR 2
-#define LEFT_BACK_MOTOR 3
+// Motor ports (your latest)
+#define RIGHT_BACK_MOTOR 1
+#define RIGHT_FRONT_MOTOR 3
+#define LEFT_FRONT_MOTOR 0
+#define LEFT_BACK_MOTOR 2
 
-// Servo ports (your original)
-#define SERVO_FRONT_RIGHT 2
-#define SERVO_FRONT_LEFT 3
+// Servo ports
+#define SERVO_FRONT_RIGHT 0
+#define SERVO_FRONT_LEFT 0
 
 #define START_LIGHT_SENSOR 3
-
-// ===================== ROBUST ENCODER MOVEMENT =====================
+#define LINE_SENSOR 1
 
 void stop() {
     mav(RIGHT_BACK_MOTOR, 0);
@@ -21,83 +21,47 @@ void stop() {
     mav(LEFT_BACK_MOTOR, 0);
 }
 
-void move_forward_ticks(int speed, int ticks) {
-    cmcp(RIGHT_BACK_MOTOR);
-    cmcp(RIGHT_FRONT_MOTOR);
-    cmcp(LEFT_FRONT_MOTOR);
-    cmcp(LEFT_BACK_MOTOR);
-    
-    while (gmcp(LEFT_FRONT_MOTOR) < ticks) {
-        mav(RIGHT_BACK_MOTOR,   speed);
-        mav(RIGHT_FRONT_MOTOR, -speed);
-        mav(LEFT_FRONT_MOTOR,   speed);
-        mav(LEFT_BACK_MOTOR,   -speed);
-    }
-    stop();
+void forward(int speed, int time) {
+    mav(RIGHT_BACK_MOTOR,  speed);
+    mav(RIGHT_FRONT_MOTOR, speed);
+    mav(LEFT_FRONT_MOTOR,  speed);
+    mav(LEFT_BACK_MOTOR,   speed);
+    msleep(time);
 }
 
-void move_backward_ticks(int speed, int ticks) {
-    cmcp(RIGHT_BACK_MOTOR);
-    cmcp(RIGHT_FRONT_MOTOR);
-    cmcp(LEFT_FRONT_MOTOR);
-    cmcp(LEFT_BACK_MOTOR);
-    
-    int target = -ticks;
-    while (gmcp(LEFT_FRONT_MOTOR) > target) {
-        mav(RIGHT_BACK_MOTOR,  -speed);
-        mav(RIGHT_FRONT_MOTOR,  speed);
-        mav(LEFT_FRONT_MOTOR,  -speed);
-        mav(LEFT_BACK_MOTOR,    speed);
-    }
-    stop();
+void backward(int speed, int time) {
+    mav(RIGHT_BACK_MOTOR,  -speed);
+    mav(RIGHT_FRONT_MOTOR, -speed);
+    mav(LEFT_FRONT_MOTOR,  -speed);
+    mav(LEFT_BACK_MOTOR,   -speed);
+    msleep(time);
 }
 
-void right_ticks(int speed, int ticks) {     // Turn Right
-    cmcp(RIGHT_BACK_MOTOR);
-    cmcp(RIGHT_FRONT_MOTOR);
-    cmcp(LEFT_FRONT_MOTOR);
-    cmcp(LEFT_BACK_MOTOR);
-    
-    while (gmcp(LEFT_FRONT_MOTOR) < ticks) {
-        mav(RIGHT_BACK_MOTOR,  -speed);
-        mav(RIGHT_FRONT_MOTOR,  speed);
-        mav(LEFT_FRONT_MOTOR,   speed);
-        mav(LEFT_BACK_MOTOR,   -speed);
-    }
-    stop();
+void right(int speed, int time) {
+    mav(RIGHT_BACK_MOTOR,  speed);
+    mav(RIGHT_FRONT_MOTOR, speed);
+    mav(LEFT_FRONT_MOTOR, -speed);
+    mav(LEFT_BACK_MOTOR,   speed);
+    msleep(time);
 }
 
-void left_ticks(int speed, int ticks) {      // Turn Left
-    cmcp(RIGHT_BACK_MOTOR);
-    cmcp(RIGHT_FRONT_MOTOR);
-    cmcp(LEFT_FRONT_MOTOR);
-    cmcp(LEFT_BACK_MOTOR);
-    
-    while (gmcp(LEFT_FRONT_MOTOR) > -ticks) {
-        mav(RIGHT_BACK_MOTOR,   speed);
-        mav(RIGHT_FRONT_MOTOR, -speed);
-        mav(LEFT_FRONT_MOTOR,  -speed);
-        mav(LEFT_BACK_MOTOR,    speed);
-    }
-    stop();
+void left(int speed, int time) {
+    mav(RIGHT_BACK_MOTOR, -speed);
+    mav(RIGHT_FRONT_MOTOR, -speed);
+    mav(LEFT_FRONT_MOTOR,  speed);
+    mav(LEFT_BACK_MOTOR,   speed);
+    msleep(time);
 }
 
-void uturn_ticks(int speed, int ticks) {
-    cmcp(RIGHT_BACK_MOTOR);
-    cmcp(RIGHT_FRONT_MOTOR);
-    cmcp(LEFT_FRONT_MOTOR);
-    cmcp(LEFT_BACK_MOTOR);
-    
-    while (gmcp(LEFT_FRONT_MOTOR) < ticks) {
-        mav(RIGHT_BACK_MOTOR,  -speed);
-        mav(RIGHT_FRONT_MOTOR,  speed);
-        mav(LEFT_FRONT_MOTOR,   speed);
-        mav(LEFT_BACK_MOTOR,   -speed);
-    }
-    stop();
+void uturn(int speed, int time){
+    mav(RIGHT_BACK_MOTOR, 0);
+    mav(RIGHT_FRONT_MOTOR, 0);
+    mav(LEFT_FRONT_MOTOR, speed);
+    mav(LEFT_BACK_MOTOR, speed);
+    msleep(time);
 }
 
-// ===================== DOZER =====================
+// Dozer
 void dozer(int position, int time){
     enable_servos();
     set_servo_position(SERVO_FRONT_RIGHT, position);
@@ -110,62 +74,137 @@ void dozerup(){
 }
 
 void dozerdown(){
-    dozer(975, 900);
+    dozer(995, 900);
 }
 
 int main() {
-    //wait_for_light(START_LIGHT_SENSOR);   // Uncomment for tournament
+    //wait_for_light(START_LIGHT_SENSOR);
     shut_down_in(118);
-    
+   
     // === START SEQUENCE ===
-    dozerup();           // Raise bulldozer
-    dozerdown();         // Lower to scooping position
-    
-    // === MAIN ROUTINE - Conservative speeds for old batteries ===
-    move_forward_ticks(750, 7200);     // Forward past first line
-    msleep(400);
-    
-    right_ticks(650, 1550);            // Turn right
+    dozerdown();
     msleep(300);
-    
-    move_forward_ticks(700, 7600);     // Scoop blue poms + cones
-    msleep(400);
-    
-    uturn_ticks(700, 2050);            // U-turn
-    msleep(400);
-    
-    move_forward_ticks(700, 8100);     // Scoop orange poms
-    msleep(400);
-    
-    left_ticks(650, 1420);             // Turn left
+    dozerup();
     msleep(300);
-    
-    move_forward_ticks(700, 3900);     // Into Lower Start Box
+   
+    // Drive forward past first black line
+    forward(900, 4550);
+    msleep(330);
+   
+    // Turn right toward the pom/cone line
+    right(800, 300);
+    msleep(100);
+
+    forward(900, 300);
+    msleep(200);
+
+    right(800, 900);
+    msleep(300);
+
+    forward(850, 1000);
+    msleep(300);
+
+    right(800, 100);
+    msleep(300);
+
+    // Drive forward scooping blue poms + cones
+    forward(850, 1000);
+    msleep(300);
+
+    right(800, 55);
+    msleep(300);
+
+    forward(850, 4200);
+    msleep(300);
+
+    forward(850, 4200);
+    msleep(300);
+
+    right(800, 10);
+    msleep(200);
+
+    forward(1050, 3950);
+    msleep(700);
+
+    right(800, 450);
+    msleep(300);
+    forward(800, 500);
+    msleep(200);
+    right(800, 2600);
+    msleep(1300);
+    stop();
+
+    ao();
+    msleep(2000);
+   
+    // Return / rest of sequence
+    forward(850, 3000);
+    msleep(500);
+
+    left(800, 200);
+
+    forward(900, 7500);
+    msleep(500);
+
+    right(800, 400); 
+
+    forward(900, 7500);
+    msleep(7000);
+
+    // Turn left at end of board
+    left(800, 1050);
+    msleep(500);
+   
+    // Drive into Lower Start Box
+    forward(800, 5000);
+    msleep(5000);
+   
+    // Semi-raise dozer to drop poms and cone
+    dozer(1650, 600);
     msleep(400);
-    
-    // Drop poms & cone
-    dozer(1650, 800);
-    msleep(600);
-    
+
+    forward(800, 2000);
+    msleep(800);
+   
     // === KNOCK DOWN 3-CUBE TOWER ===
-    move_forward_ticks(850, 1000);     // Ram front wall
-    msleep(300);
-    
-    dozer(1950, 600);
+    backward(900, 4550);
+    msleep(4000);
+
+    dozerup();
+
+    left(1200, 2400);
+    msleep(1200);
+
+    ao();
+    msleep(1000);
+
+    forward(850, 4200);
+    msleep(800);
+
+    right(850, 700);
+    msleep(600);
+
+    forward(850, 2000);
+    msleep(600);
+
+    right(850, 300);
+    msleep(600);
+   
+    forward(850, 3200);
     msleep(400);
-    
-    left_ticks(550, 580);              // Slight left knock
+    left(800, 1050);
+    msleep(500);
+   
+    forward(800, 2400);
     msleep(300);
-    
-    move_backward_ticks(700, 750);
-    msleep(300);
-    
-    move_forward_ticks(850, 1150);     // Final square up
-    
-    // Final park
+   
+    dozer(1650, 600);
+    dozerdown();
+    msleep(400);
+
     dozerup();
     stop();
     disable_servos();
-    
+   
     return 0;
 }
